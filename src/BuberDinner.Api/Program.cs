c4faf1d5df42cff1972aba639dcc8dd6e17c5077
duyanhvuong2001 <102603197@student.swin.edu.au>
2024-03-08 16:08:45 +1100

    Update Program.cs

--- src/BuberDinner.Api/Program.cs
+++ src/BuberDinner.Api/Program.cs
@@ -20,11 +20,11 @@ builder.Services
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 
 app.UseExceptionHandler("/error");
